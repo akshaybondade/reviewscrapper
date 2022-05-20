e8e7f951b0025399e0a5dc04bf757895a38c4a4f
akshaybondade <bondadeakshay@gmail.com>
2022-05-21 03:02:31 +0530

    first commit

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
             flipkart_html = bs(flipkartPage, "html.parser")
             bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
             del bigboxes[0:3]
-            box = bigboxes[0]
+
             productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
